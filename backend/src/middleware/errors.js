da0eb13c25d150aaeaa163d4b021d39cf5c11677
Will <william.shuppert@gmail.com>
2023-09-09 10:31:23 -0400

    add error converter

--- backend/src/middleware/errors.js
+++ backend/src/middleware/errors.js
@@ -1,5 +1,22 @@
+import { ApiError } from "../utils/api-error.js"
+
+export const errorConverter = (err, req, res, next) => {
+    let error = err
+    if (!(error instanceof ApiError)) {
+        const statusCode = error.statusCode || httpStatus.INTERNAL_SERVER_ERROR
+        const message = error.message || httpStatus[statusCode]
+        error = new ApiError(statusCode, message, false, err.stack)
+    }
+    next(error)
+}
+
 export const errorHandler = (err, req, res, next) => {
-    let { statusCode, message } = err;
+    let { statusCode, message } = err
+
+    if (config.env === 'production' && !err.isOperational) {
+        statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+        message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+    }
 
     if (process.env.NODE_ENV === 'development')
         console.log(err)
