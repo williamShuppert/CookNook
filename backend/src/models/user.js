9c4241170e1b06d1cd6ffff5ccb16e122a21b9e7
Will <william.shuppert@gmail.com>
2023-09-09 10:32:02 -0400

    create user model

---
+++ backend/src/models/user.js
@@ -0,0 +1,24 @@
+import sequelize from '../config/sequelize.js'
+import { DataTypes } from 'sequelize'
+
+const User = sequelize.define('User', {
+    id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    displayname: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING
+    }
+}, {})//.sync({alter:true})
+
+export default User
