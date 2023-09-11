675ddf2f9683237e275aff3daecfaf3beddf1253
Will <william.shuppert@gmail.com>
2023-09-11 08:01:51 -0400

    add token service

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,11 +2,11 @@ import { catchAsync } from "../utils/catch-async.js"
 import { ApiError } from "../utils/api-error.js"
 import httpStatus from 'http-status'
 import UserService from '../services/user.js'
-import jwt from "jsonwebtoken"
+import tokenService from "../services/token.js"
 
 export default {
     sendAuthTokens: catchAsync(async (req, res) => {
-        const token = jwt.sign({ userId: req.user.id }, process.env.ACCESS_JWT_SECRET, { expiresIn: '1h' }) // TODO: move to service
+        const token = tokenService.generateToken(req.user.id, 'access')
         res.setHeader('Authorization', `Bearer ${token}`)
         res.json({
             id: req.user.id,
---
+++ backend/services/token.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+export const generateToken = (userId, type, payload = {}) => {
+    if (type != 'access' || type != 'refresh')
+        throw new Error(`type for generating token is not 'access' or 'refresh'`)
+    
+    let secret;
+    let expiresIn
+    if (type == 'access') {
+        expiresIn = '30min'
+        secret = process.ACCESS_JWT_SECRET            
+    }
+    else if (type == 'refresh') {
+        expiresIn = '1h'
+        secret = process.REFRESH_JWT_SECRET
+    }
+
+    return jwt.sign({...payload, userId, type}, secret, { expiresIn })
+}
+
+export default {
+    generateToken
+}
