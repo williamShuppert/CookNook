78c9406ba96e5083ca78a77f08e67a2a7c929b6f
William Shuppert <william.shuppert@gmail.com>
2023-09-12 10:19:27 -0400

    remove sequelize from index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,7 @@
 import '../config/env.config.js'
 import './config/mysql2.js'
 import app from './app.js'
-import sequelize from './config/sequelize.js'
 
 const port = process.env.PORT
 
-try {
-    await sequelize.authenticate();
-    app.listen(port, () => console.log(`listening on port ${port}`))
-    console.log('Connection has been established successfully.')
-} catch (error) {
-    console.error('Unable to connect to the database:', error)
-}
+app.listen(port, () => console.log(`listening on port ${port}`))
