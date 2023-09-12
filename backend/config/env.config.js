267a7a2c49ba83e12dc0f003b5274a71563688c4
William Shuppert <william.shuppert@gmail.com>
2023-09-12 10:47:21 -0400

    improve .env config errors

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv'
 import Joi from 'joi'
+import fs from 'fs'
 
-if (!process.env.NODE_ENV) throw new Error('NODE_ENV needs to be set')
+const NODE_ENV = process.env.NODE_ENV
+const envFileName = `.env.${NODE_ENV}`
+if (!NODE_ENV) throw new Error('NODE_ENV needs to be set')
+if (!fs.existsSync(envFileName)) throw new Error(`Missing ${envFileName} file`)
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+dotenv.config({ path: envFileName })
 
 const envVarsSchema = Joi.object().keys({
     NODE_ENV: Joi.string().valid('prod', 'dev', 'test').required(),
@@ -15,11 +19,16 @@ const envVarsSchema = Joi.object().keys({
     REFRESH_JWT_SECRET: Joi.string().min(64).required(),
 }).unknown()
 
-const { error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env)
+const { error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env, {abortEarly: false})
 
 if (error)
-    throw new Error(`Config validation error: ${error.message}`)
+    throw new Error(
+        "Config validation error\n" +
+        `The ${envFileName} file fails validation:\n` +
+        error.details.map(detail => ' - ' + detail.message).join('\n') +
+        '\n'
+    )
 
 console.log('Environment Config: (located at: /config/env.config.js)')
-console.log(` - Current Environment: "${process.env.NODE_ENV}"`)
+console.log(` - Current Environment: "${NODE_ENV}"`)
 console.log(` - Current Database: "${process.env.DB_NAME}"`)
