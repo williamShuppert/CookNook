1a526733e70fe091fea2e7757c22fe4be983d05d
William Shuppert <william.shuppert@gmail.com>
2023-09-12 11:26:01 -0400

    add swagger for /auth/local

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -19,3 +19,52 @@ router.route('/protected')
     .get(passport.authenticate('jwt', {session: false}), (req, res) => res.json({message:'just look at all these secrets...'}))
 
 export default router
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: User authorization
+ */
+
+/**
+ * @swagger
+ * /auth/local:
+ *   post:
+ *     summary: Login in with a username and password
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *             example:
+ *               username: fake
+ *               password: password1
+ *     responses:
+ *       "200":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "401":
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Unauthorized'
+ */
