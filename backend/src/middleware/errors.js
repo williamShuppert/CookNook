379d965af57cfbd19de1779cec5f9c71956bf3ed
Will <william.shuppert@gmail.com>
2023-09-12 12:37:29 -0400

    c8 coverage and test fix

--- backend/src/middleware/errors.js
+++ backend/src/middleware/errors.js
@@ -4,8 +4,8 @@ import httpStatus from "http-status"
 export const errorConverter = (err, req, res, next) => {
     let error = err
     if (!(error instanceof ApiError)) {
-        const statusCode = error.statusCode || httpStatus.INTERNAL_SERVER_ERROR
-        const message = error.message || httpStatus[statusCode]
+        const statusCode = httpStatus.INTERNAL_SERVER_ERROR
+        const message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR]
         error = new ApiError(statusCode, message, false, err.stack)
     }
     next(error)
@@ -14,17 +14,19 @@ export const errorConverter = (err, req, res, next) => {
 export const errorHandler = (err, req, res, next) => {
     let { statusCode, message } = err
 
-    if (process.env.NODE_ENV === 'prod' && !err.isOperational) {
+    if (process.env.NODE_ENV !== 'dev' && !err.isOperational) {
         statusCode = httpStatus.INTERNAL_SERVER_ERROR;
         message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
     }
 
-    if (process.env.NODE_ENV !== 'prod' && !err.isOperational)
+    /* c8 ignore next */
+    if (process.env.NODE_ENV === 'dev' && !err.isOperational)
         console.log(err)
 
     const response = {
         code: statusCode,
         message,
+        /* c8 ignore next */
         ...(process.env.NODE_ENV === 'dev' && { stack: err.stack }),
     }
 
