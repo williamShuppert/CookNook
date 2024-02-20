f746bc711d26002129be316eb72b12afc755224b
Will <william.shuppert@gmail.com>
2024-02-20 12:06:08 -0500

    create redux recipes slice

--- frontend/src/components/pages/SearchPage/index.jsx
+++ frontend/src/components/pages/SearchPage/index.jsx
@@ -1,100 +1,27 @@
 import './style.scss'
 import RecipeCard from './RecipeCard'
 import { motion } from 'framer-motion'
-import { useCallback, useEffect, useState } from 'react'
-import porkBowl from '/pork-bowl.jpg'
-import parmesanChickenImage from '/Crispy-Parmesan-chicken.avif'
-import pastaParmesanImage from '/Pasta-Parmesan.jpg'
-import chickenOrzoImage from '/Winner-winner-chicken-orzo-dinner.avif'
-import koreanBeefImage from '/Korean-beef-bibimbap.avif'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useCallback, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import SearchInput from './SearchInput'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchRecipes, recipeUpdated, selectRecipes, selectRecipesStatus } from '../../../features/recipes/recipesSlice'
 
 const SearchPage = () => {
-    const location = useLocation()
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const [recipes, setRecipes] = useState([{
-            id: 1,
-            name: 'Moo Shu Pork Bowl',
-            author: 'Hello Fresh',
-            description: 'Lorem ipsum dolor sit amet consectetur. Ipsum sit dolor ac risus duis et ultrices commodo. Proin pharetra eget congue quis nunc eget. Id quis fermentum dolor commodo felis est amet aliquet tellus. Ultricies facilisi at sit at vitae morbi leo elementum. Est ipsum sagittis et vulputate ac vitae vitae etiam et. Sit pulvinar sit nulla netus a scelerisque integer. Enim euismod viverra fermentum tristique eget. Lectus sit at volutpat ut.',
-            difficulty: 'easy',
-            minutes: 20,
-            calories: 750,
-            rating: 4.8,
-            ratings: 40,
-            bookmarks: 28,
-            userRating: -1,
-            userBookmarked: false,
-            image: porkBowl
-        }, {
-            id: 2,
-            name: 'Crispy Parmesan Chicken',
-            author: 'Hello Fresh',
-            description: 'Lorem ipsum dolor sit amet consectetur. Ipsum sit dolor ac risus duis et ultrices commodo. Proin pharetra eget congue quis nunc eget. Id quis fermentum dolor commodo felis est amet aliquet tellus. Ultricies facilisi at sit at vitae morbi leo elementum. Est ipsum sagittis et vulputate ac vitae vitae etiam et. Sit pulvinar sit nulla netus a scelerisque integer. Enim euismod viverra fermentum tristique eget. Lectus sit at volutpat ut.',
-            difficulty: 'easy',
-            minutes: 20,
-            calories: 750,
-            rating: 4.6,
-            ratings: 40,
-            bookmarks: 58,
-            userRating: -1,
-            userBookmarked: false,
-            image: parmesanChickenImage
-        }, {
-            id: 3,
-            name: 'Pasta Parmesan',
-            author: 'Hello Fresh',
-            description: 'Lorem ipsum dolor sit amet consectetur. Ipsum sit dolor ac risus duis et ultrices commodo. Proin pharetra eget congue quis nunc eget. Id quis fermentum dolor commodo felis est amet aliquet tellus. Ultricies facilisi at sit at vitae morbi leo elementum. Est ipsum sagittis et vulputate ac vitae vitae etiam et. Sit pulvinar sit nulla netus a scelerisque integer. Enim euismod viverra fermentum tristique eget. Lectus sit at volutpat ut.',
-            difficulty: 'easy',
-            minutes: 20,
-            calories: 750,
-            rating: 4.9,
-            ratings: 40,
-            bookmarks: 41,
-            userRating: -1,
-            userBookmarked: false,
-            image: pastaParmesanImage
-        }, {
-            id: 4,
-            name: 'Winner Winner Chicken Orzo Dinner',
-            description: 'Lorem ipsum dolor sit amet consectetur. Ipsum sit dolor ac risus duis et ultrices commodo. Proin pharetra eget congue quis nunc eget. Id quis fermentum dolor commodo felis est amet aliquet tellus. Ultricies facilisi at sit at vitae morbi leo elementum. Est ipsum sagittis et vulputate ac vitae vitae etiam et. Sit pulvinar sit nulla netus a scelerisque integer. Enim euismod viverra fermentum tristique eget. Lectus sit at volutpat ut.',
-            author: 'Hello Fresh',
-            difficulty: 'medium',
-            minutes: 20,
-            calories: 750,
-            rating: 4.2,
-            ratings: 40,
-            bookmarks: 36,
-            userRating: -1,
-            userBookmarked: false,
-            image: chickenOrzoImage
-        }, {
-            id: 5,
-            name: 'Korean Beef Bibimbap',
-            description: 'Lorem ipsum dolor sit amet consectetur. Ipsum sit dolor ac risus duis et ultrices commodo. Proin pharetra eget congue quis nunc eget. Id quis fermentum dolor commodo felis est amet aliquet tellus. Ultricies facilisi at sit at vitae morbi leo elementum. Est ipsum sagittis et vulputate ac vitae vitae etiam et. Sit pulvinar sit nulla netus a scelerisque integer. Enim euismod viverra fermentum tristique eget. Lectus sit at volutpat ut.',
-            author: 'Hello Fresh',
-            difficulty: 'medium',
-            minutes: 20,
-            calories: 750,
-            rating: 4.2,
-            ratings: 40,
-            bookmarks: 36,
-            userRating: -1,
-            userBookmarked: false,
-            image: koreanBeefImage
-        }
-    ])
+    const recipes = useSelector(selectRecipes)
+    const recipesStatus = useSelector(selectRecipesStatus)
+
+    useEffect(() => {
+        if (recipesStatus == 'idle')
+            dispatch(fetchRecipes())
+    }, [dispatch, recipesStatus])
+
     
     const setRecipe = useCallback((updatedRecipe) => {
-        const index = recipes.findIndex(recipe => recipe.id == updatedRecipe.id)
-        
-        setRecipes(recipes => [
-            ...recipes.slice(0, index),
-            updatedRecipe,
-            ...recipes.slice(index + 1),
-        ])
+        dispatch(recipeUpdated(updatedRecipe)) 
     }, [])
 
     const handleRecipeClick = useCallback(recipe => {
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import recipeReducer from '../features/recipes/recipesSlice'
 
 export default configureStore({
     reducer: {
+        recipes: recipeReducer,
         counter: counterReducer,
     },
 })
