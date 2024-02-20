f1c9e9613133a5219b65402d52be51e3eb58fd59
Will <william.shuppert@gmail.com>
2024-02-20 08:48:40 -0500

    use hash router to work with gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import NavBar from './components/common/NavBar/index.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store'
@@ -10,10 +10,10 @@ import store from './redux/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/CookNook/">
+      <HashRouter basename="/CookNook/">
         <App />
         <NavBar />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
