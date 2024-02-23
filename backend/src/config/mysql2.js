dac9399beaba40b8cb073c0c9d58f66e5a6314ee
shuppewd <shuppewd@miamioh.edu>
2024-02-23 13:14:13 -0500

    move mysql2 helper/wrapper out of middleware

--- backend/src/config/mysql2.js
+++ backend/src/config/mysql2.js
@@ -7,3 +7,34 @@ export const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 })
+
+export const useConnection = (conn) => ({
+    conn, 
+
+    sproc: async (sprocName, values, expectOne) => {
+        const res = (await conn.execute(`call ${sprocName}(${values.map(()=>'?').join(',')})`, values))[0][0]
+        return expectOne ? res[0] : res
+    },
+    
+    query: async (query, expectOne) => {
+        const res = (await conn.execute(query))[0]
+        return expectOne ? res[0] : res;
+    },
+    
+    execute: async (query, values, expectOne) => {
+        const res = (await conn.execute(query, values))[0]
+        return expectOne ? res[0] : res;
+    },
+
+    transaction: async (func) => {
+        await conn.beginTransaction()
+        try {
+            const res = await func()
+            await conn.commit()
+            return res
+        } catch (error) {
+            await conn.rollback()
+            throw error
+        }
+    }
+})
--- backend/src/middleware/mysql2.js
+++ backend/src/middleware/mysql2.js
@@ -1,39 +1,8 @@
-import { pool } from '../config/mysql2.js'
+import { pool, useConnection } from '../config/mysql2.js'
 
 export const useDB = () => async (req, res, next) => {
     const conn = await pool.getConnection()
-
-    req.db = {
-        conn, 
-
-        sproc: async (sprocName, values, expectOne) => {
-            const res = (await conn.execute(`call ${sprocName}(${values.map(()=>'?').join(',')})`, values))[0][0]
-            return expectOne ? res[0] : res
-        },
-        
-        query: async (query, expectOne) => {
-            const res = (await conn.execute(query))[0]
-            return expectOne ? res[0] : res;
-        },
-        
-        execute: async (query, values, expectOne) => {
-            const res = (await conn.execute(query, values))[0]
-            return expectOne ? res[0] : res;
-        },
-
-        transaction: async (func) => {
-            await conn.beginTransaction()
-            try {
-                const res = await func()
-                await conn.commit()
-                return res
-            } catch (error) {
-                await conn.rollback()
-                throw error
-            }
-        }
-    }
-
+    req.db = useConnection(conn)
     res.on('finish', () => conn.release())
     next()
 }
