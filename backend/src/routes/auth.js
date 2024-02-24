a35efd8f895831d232dd9a474fe57fbad2126d62
shuppewd <shuppewd@miamioh.edu>
2024-02-24 10:06:31 -0500

    protect logout route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,12 +4,13 @@ import { loginSuccess, logout } from "../controllers/auth.js"
 import { localLoginValidation } from "../validation/auth.js"
 import { validate } from "../middleware/validate.js"
 import { usePassport } from "../config/passport.js"
+import { protect } from "../middleware/auth.js"
 
 const router = Router()
 
 router.post('/local', validate(localLoginValidation), useDB(), usePassport('local'), loginSuccess())
 
-router.post('/logout', useDB(), logout())
+router.post('/logout', protect(), useDB(), logout())
 
 router.get('/google', usePassport('google', { scope: ['email', 'profile']}))
 router.get('/google/callback', useDB(), usePassport('google'), loginSuccess())
