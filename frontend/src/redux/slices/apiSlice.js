0763dcc5d6f7f108b6a88be7329f83a50efa119a
shuppewd <shuppewd@miamioh.edu>
2024-02-24 18:24:29 -0500

    create api slice

---
+++ frontend/src/redux/slices/apiSlice.js
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { removeUser, setUser } from './authSlice'
+import httpStatus from 'http-status'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:3000',
+    credentials: 'include'
+})
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions)
+
+    if (result?.error?.status === httpStatus.UNAUTHORIZED
+        && !result.meta?.request?.url?.includes("auth") // don't auto attempt refresh when requesting an auth endpoint 
+    ) {
+        // Attempt to refresh auth tokens
+        const refreshResult = await baseQuery({
+                url: '/auth/refresh',
+                method: 'POST'
+            },
+            api,
+            extraOptions
+        )
+
+        if (refreshResult?.data) {
+            api.dispatch(setUser({...refreshResult.data}))
+            // Retry original query with newly refreshed tokens
+            result = await baseQuery(args, api, extraOptions)
+        }
+        else {
+            api.dispatch(removeUser())
+        }
+    }
+
+    return result
+}
+
+export const apiSlice = createApi({
+    baseQuery: baseQueryWithReauth,
+    endpoints: builder => ({})
+})
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/counter/counterSlice'
 import recipeReducer from './slices/searchPageSlice'
+import authReducer from './slices/authSlice'
+import { apiSlice } from './slices/apiSlice'
 
 export default configureStore({
     reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
         recipes: recipeReducer,
         counter: counterReducer,
     },
+    middleware: getDefault => getDefault()
+        .concat(apiSlice.middleware),
+    devTools: true
 })
