6171ebb02ceabc7340d5515cdc79051a43facd43
shuppewd <shuppewd@miamioh.edu>
2024-02-24 09:23:04 -0500

    remove unnessicary async

--- backend/src/config/passport-local.js
+++ backend/src/config/passport-local.js
@@ -5,7 +5,7 @@ export default new Strategy({
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true
-}, async (req, username, password, done) => {
+}, (req, username, password, done) => {
     AuthService(req.db).localLogin(username, req.body.email, password)
         .then(user => done(null, user))
         .catch(err => done(err))
