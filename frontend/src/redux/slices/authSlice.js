0172f1906c58d798fe98a1d357d94ff260ab12f7
shuppewd <shuppewd@miamioh.edu>
2024-02-24 18:24:03 -0500

    create auth slice

---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null
+    },
+    reducers: {
+        setUser: (state, action) => { state.user = action.payload.user },
+        removeUser: state => { state.user = false }
+    },
+})
+
+export const { setUser, removeUser } = authSlice.actions
+
+export default authSlice.reducer
+
+export const selectUser = state => state.auth.user
