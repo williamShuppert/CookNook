29481942ac9d666d0a3f5bd05f8a11401737b24e
Will Shuppert <william.shuppert@gmail.com>
2024-02-25 14:30:22 -0500

    add roles middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -11,10 +11,22 @@ export const protect = () => (req, res, next) => {
 
     try {
         const decoded = JWT.verify(accessJWT, process.env.ACCESS_JWT_SECRET)
-        req.user = { id: decoded.userId }
+        req.user = {
+            id: decoded.userId,
+            roles: decoded.roles ?? []
+        }
     } catch {
         throw new ApiError(httpStatus.UNAUTHORIZED)
     }
 
     next()
 }
+
+export const requireRoles = (requiredRoles) => [protect(), (req, res, next) => {
+    const hasAllRoles = requiredRoles.every(role => req.roles.includes(role))
+
+    if (!hasAllRoles)
+        throw new ApiError(httpStatus.FORBIDDEN, 'Insufficient permissions')
+
+    next()
+}]
