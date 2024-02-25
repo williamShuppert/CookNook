134150c66673d454038438db2a06c5e00507c919
Will Shuppert <william.shuppert@gmail.com>
2024-02-25 14:03:50 -0500

    remove test routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,7 @@ import helmet from 'helmet'
 import cors from 'cors'
 import helmetOptions from './config/helmet.js'
 import cookieParser from 'cookie-parser'
-import { useDB } from './middleware/mysql2.js'
 import { errorConverter, errorHandler } from './middleware/errors.js'
-import { catchAsync } from './utils/catchAsync.js'
 import usersRoute from './routes/users.js'
 import authRoute from './routes/auth.js'
 import passport from 'passport'
@@ -28,12 +26,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
-
-// Test routes
-app.get('/api', (req, res) => res.json({message: 'hello world'}))
-app.get('/api/db', useDB(), catchAsync(async (req, res) => {
-    res.json({queryResult: await req.db.query('SELECT 1+1 AS ans', true) })
-}))
 
 // Routes
 app.use('/api/users', usersRoute)
