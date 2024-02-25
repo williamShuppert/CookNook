03072c3f791a4b7f21bfd4631b86df3899c64b3f
Will <william.shuppert@gmail.com>
2024-02-24 19:54:35 -0500

    allow create user without email

--- backend/src/validation/users.js
+++ backend/src/validation/users.js
@@ -6,7 +6,7 @@ export const validUsername = Joi.string().min(3).max(50)
 
 export const createUserValidation = Joi.object().keys({
     body: Joi.object().keys({
-        email: validEmail.required(),
+        email: validEmail.allow(null),
         username: validUsername.required(),
         password: validPassword.required()
     })
