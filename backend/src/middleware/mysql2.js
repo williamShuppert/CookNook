66f145a6284e4afd69950c7f4b3ed051d893398a
Will Shuppert <william.shuppert@gmail.com>
2024-02-27 11:55:53 -0500

    add exception handling to db middleware

--- backend/src/middleware/mysql2.js
+++ backend/src/middleware/mysql2.js
@@ -1,8 +1,12 @@
 import { pool, useConnection } from '../config/mysql2.js'
 
 export const useDB = () => async (req, res, next) => {
-    const conn = await pool.getConnection()
-    req.db = useConnection(conn)
-    res.on('finish', () => conn.release())
-    next()
+    try {
+        const conn = await pool.getConnection()
+        req.db = useConnection(conn)
+        res.on('finish', () => conn.release())
+        next()
+    } catch (err) {
+        next(err)
+    }
 }
