99f3ade4f756b23ae08f5696f63cbeacb9f1052b
Will Shuppert <william.shuppert@gmail.com>
2024-03-04 23:33:40 -0500

    allow question marks to be used rather than pg's positional params

--- backend/src/middleware/pg.js
+++ backend/src/middleware/pg.js
@@ -3,7 +3,16 @@ import { pool } from '../config/pg.js'
 export const useDB = () => async (req, res, next) => {
     try {
         const client = await pool.connect()
+
         req.db = client
+        req.db.dynamicQuery = (query, values) => {
+            // allow question marks to be used rather than pg's positional params ('$1' can now just be '?')
+            let valueIndex = 1
+            return client.query(
+                query.replace(/\?/g, _ => `$${valueIndex++}`),
+                values)
+        }
+
         res.on('finish', () => client.release())
         next()
     } catch (err) {
