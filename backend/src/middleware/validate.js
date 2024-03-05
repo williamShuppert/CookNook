bc37a37dea69af7b8432eacfcf1f48623fb3d07a
Will Shuppert <william.shuppert@gmail.com>
2024-03-04 23:39:23 -0500

    Assign validated req values to original req object

--- backend/src/middleware/validate.js
+++ backend/src/middleware/validate.js
@@ -1,10 +1,16 @@
 import httpStatus from "http-status"
 
 export const validate = (schema) => (req, res, next) => {
-    const { error } = schema.validate(req, { abortEarly: false, allowUnknown: true })
+    const { value, error } = schema.validate(req, { abortEarly: false, allowUnknown: true })
     if (error) {
         const errorMessage = error.details.map((detail) => detail.message).join(', ')
         return res.status(httpStatus.BAD_REQUEST).json({ code: httpStatus.BAD_REQUEST, message: errorMessage })
     }
+    
+    // Assign validated req values to original req object
+    Object.keys(value).forEach(key => {
+        req[key] = value[key]
+    })
+
     next()
 }
