f58b48304c99116e5efcf9fc4fc4f3017a595bb9
Will Shuppert <william.shuppert@gmail.com>
2024-07-13 21:04:00 -0400

    document interval formatting options

--- backend/src/config/pg.js
+++ backend/src/config/pg.js
@@ -1,5 +1,7 @@
 import pg from 'pg'
-const { Pool } = pg
+const { Pool, types } = pg
+
+// types.setTypeParser(1186, value => value) // keep intervals unformatted
 
 export const pool = process.env.DB_URL
     ? new Pool({ connectionString: process.env.DB_URL })
